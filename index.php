db61d0d86f4e1cb09f3832f2c4088e34a7792ccf
Aldo Vernando <40777887+AldoVernando@users.noreply.github.com>
2019-04-22 08:40:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -109,7 +109,6 @@ use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=submission;AccountKey=m7qQe3NPr50h0VEdckYmQXo6rFBHTmU50g4yan9Aq+Ye+Gqg7tsUR/w1CPOJLUKlnNsz+bQ9sa/mEOxT29BLig==";
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
-// $fileToUpload = "HelloWorld.txt";
 // if (!isset($_GET["Cleanup"])) {
     // Create container options object.
     // $createContainerOptions = new CreateContainerOptions();
@@ -119,7 +118,6 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
     // $createContainerOptions->addMetaData("key1", "value1");
     // $createContainerOptions->addMetaData("key2", "value2");
     $containerName = "blockblobs";
-    try {
         // Create container.
         // $blobClient->createContainer($containerName, $createContainerOptions);
         // Getting local file so that we can upload it to Azure.
@@ -141,7 +139,7 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
 //         $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 //         // List blobs.
         $listBlobsOptions = new ListBlobsOptions();
-//         $listBlobsOptions->setPrefix("HelloWorld");
+        $listBlobsOptions->setPrefix("");
         echo "These are the blobs present in the container: ";
         do{
             $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
@@ -160,45 +158,4 @@ $blobClient = BlobRestProxy::createBlobService($connectionString);
             $listBlobsOptions->setContinuationToken($result->getContinuationToken());
         } while($result->getContinuationToken());
         echo "<br />";
-//         // Get blob.
-//         echo "This is the content of the blob uploaded: ";
-//         $blob = $blobClient->getBlob($containerName, $fileToUpload);
-//         fpassthru($blob->getContentStream());
-//         echo "<br />";
-    }
-    catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-    catch(InvalidArgumentTypeException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-} 
-else 
-{
-    try{
-        // Delete container.
-        echo "Deleting Container".PHP_EOL;
-        echo $_GET["containerName"].PHP_EOL;
-        echo "<br />";
-        $blobClient->deleteContainer($_GET["containerName"]);
-    }
-    catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-}
 ?>
